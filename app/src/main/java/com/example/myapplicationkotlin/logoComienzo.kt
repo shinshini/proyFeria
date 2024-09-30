b8e778c58d403c4a0662c81226634991788a4fde
shinshini <leonorchinchisa@gmail.com>
2024-09-30 11:40:06 -0400

    se agrego diseño

--- app/src/main/java/com/example/myapplicationkotlin/logoComienzo.kt
+++ app/src/main/java/com/example/myapplicationkotlin/logoComienzo.kt
@@ -1,38 +1,30 @@
 package com.example.myapplicationkotlin
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.os.Handler
+import android.content.Intent
 import androidx.core.view.WindowInsetsCompat
 
-class login : AppCompatActivity() {
-    lateinit var btnLogin: Button
+class logoComienzo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_logo_comienzo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        cargarR()
-        estadoButon()
+        // Temporizador para la pantalla de inicio
+        Handler().postDelayed({
+            // Intent para pasar a la siguiente actividad
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 milisegundos = 3 segundos
     }
 
-    fun cargarR() {
-        btnLogin = findViewById(R.id.btn_ingresarReg)
-    }
-
-    //estado button
-    fun estadoButon() {
-        btnLogin.setOnClickListener {
-            var aa = Intent(this, MainActivity::class.java)
-            startActivity(aa)
-        }
-    }
 }
